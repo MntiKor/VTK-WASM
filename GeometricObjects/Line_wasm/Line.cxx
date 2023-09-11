71fec71021cdc6176be7246923e888af370eb399
Adrien Stucky <adrien.stucky@kitware.com>
2023-09-11 16:38:52 +0200

    Line working

---
+++ GeometricObjects/Line_wasm/Line.cxx
@@ -0,0 +1,46 @@
+#include <vtkActor.h>
+#include <vtkLineSource.h>
+#include <vtkNamedColors.h>
+#include <vtkNew.h>
+#include <vtkPolyData.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkProperty.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+
+int main(int, char*[])
+{
+  // Create two points, P0 and P1
+  double p0[3] = {1.0, 0.0, 0.0};
+  double p1[3] = {0.0, 1.0, 0.0};
+
+  vtkNew<vtkLineSource> lineSource;
+  lineSource->SetPoint1(p0);
+  lineSource->SetPoint2(p1);
+
+  // Visualize
+  vtkNew<vtkNamedColors> colors;
+
+  vtkNew<vtkPolyDataMapper> mapper;
+  mapper->SetInputConnection(lineSource->GetOutputPort());
+  vtkNew<vtkActor> actor;
+  actor->SetMapper(mapper);
+  actor->GetProperty()->SetLineWidth(4);
+  actor->GetProperty()->SetColor(colors->GetColor3d("Peacock").GetData());
+
+  vtkNew<vtkRenderer> renderer;
+  vtkNew<vtkRenderWindow> renderWindow;
+  renderWindow->AddRenderer(renderer);
+  renderWindow->SetWindowName("Line");
+  vtkNew<vtkRenderWindowInteractor> renderWindowInteractor;
+  renderWindowInteractor->SetRenderWindow(renderWindow);
+
+  renderer->SetBackground(colors->GetColor3d("Silver").GetData());
+  renderer->AddActor(actor);
+
+  renderWindow->Render();
+  renderWindowInteractor->Start();
+
+  return EXIT_SUCCESS;
+}
