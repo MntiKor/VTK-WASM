572f29a5743aeb8dc33c8f66ab338956236c33fc
Adrien Stucky <adrien.stucky@kitware.com>
2023-09-13 11:23:09 +0200

    added addon_files to generator + animation fully working

---
+++ Animation/AnimateActors/AnimateActors.cxx
@@ -0,0 +1,120 @@
+#include "AnimateActors.h"
+
+#include <vtkAnimationCue.h>
+#include <vtkAnimationScene.h>
+#include <vtkCamera.h>
+#include <vtkCommand.h>
+#include <vtkConeSource.h>
+#include <vtkLogger.h>
+#include <vtkNamedColors.h>
+#include <vtkNew.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkProperty.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkSphereSource.h>
+
+int main(int argc, char* argv[])
+{
+  vtkLogger::Init(argc, argv);
+
+  // Colors
+  vtkNew<vtkNamedColors> colors;
+  vtkColor3d coneColor = colors->GetColor3d("Tomato");
+  vtkColor3d sphereColor = colors->GetColor3d("Banana");
+  vtkColor3d backgroundColor = colors->GetColor3d("Peacock");
+
+  // Create the graphics structure. The renderer renders into the
+  // render window.
+  vtkNew<vtkRenderWindowInteractor> iren;
+  vtkNew<vtkRenderer> ren1;
+  ren1->SetBackground(backgroundColor.GetData());
+
+  vtkNew<vtkRenderWindow> renWin;
+  iren->SetRenderWindow(renWin);
+  renWin->AddRenderer(ren1);
+
+  // Generate a sphere
+  vtkNew<vtkSphereSource> sphereSource;
+  sphereSource->SetPhiResolution(31);
+  sphereSource->SetThetaResolution(31);
+
+  vtkNew<vtkPolyDataMapper> sphereMapper;
+  sphereMapper->SetInputConnection(sphereSource->GetOutputPort());
+  vtkNew<vtkActor> sphere;
+  sphere->SetMapper(sphereMapper);
+  sphere->GetProperty()->SetDiffuseColor(sphereColor.GetData());
+  sphere->GetProperty()->SetDiffuse(.7);
+  sphere->GetProperty()->SetSpecular(.3);
+  sphere->GetProperty()->SetSpecularPower(30.0);
+
+  ren1->AddActor(sphere);
+
+  // Generate a cone
+  vtkNew<vtkConeSource> coneSource;
+  coneSource->SetResolution(31);
+
+  vtkNew<vtkPolyDataMapper> coneMapper;
+  coneMapper->SetInputConnection(coneSource->GetOutputPort());
+  // auto cone = vtkSmartPointer<vtkActor>::New();
+  vtkNew<vtkActor> cone;
+  cone->SetMapper(coneMapper);
+  cone->GetProperty()->SetDiffuseColor(coneColor.GetData());
+
+  ren1->AddActor(cone);
+
+  // Create an Animation Scene
+  vtkNew<vtkAnimationScene> scene;
+  if (argc >= 2 && strcmp(argv[1], "-real") == 0)
+  {
+    vtkLogF(INFO, "real-time mode");
+    scene->SetModeToRealTime();
+  }
+  else
+  {
+    vtkLogF(INFO, "sequence mode");
+    scene->SetModeToSequence();
+  }
+  scene->SetLoop(0);
+  scene->SetFrameRate(5);
+  scene->SetStartTime(0);
+  scene->SetEndTime(20);
+  scene->AddObserver(vtkCommand::AnimationCueTickEvent, renWin.GetPointer(),
+                     &vtkWindow::Render);
+
+  // Create an Animation Cue for each actor
+  vtkNew<vtkAnimationCue> cue1;
+  cue1->SetStartTime(5);
+  cue1->SetEndTime(23);
+  scene->AddCue(cue1);
+
+  vtkNew<vtkAnimationCue> cue2;
+  cue2->SetStartTime(1);
+  cue2->SetEndTime(10);
+  scene->AddCue(cue2);
+
+  // Create an ActorAnimator for each actor;
+  ActorAnimator animateSphere;
+  animateSphere.SetActor(sphere);
+  animateSphere.AddObserversToCue(cue1);
+
+  ActorAnimator animateCone;
+  animateCone.SetEndPosition(vtkVector3d(-1, -1, -1));
+  animateCone.SetActor(cone);
+  animateCone.AddObserversToCue(cue2);
+
+  renWin->SetWindowName("AnimateActors");
+
+  renWin->Render();
+  ren1->ResetCamera();
+  ren1->GetActiveCamera()->Dolly(.5);
+  ren1->ResetCameraClippingRange();
+
+  // Create Cue observer.
+  scene->Play();
+  scene->Stop();
+
+  iren->Start();
+  return EXIT_SUCCESS;
+}
--- Generator/GenerateExamplesWASM.sh
+++ Generator/GenerateExamplesWASM.sh
@@ -10,6 +10,8 @@ vtk_source_dir=$1
 source_dir=$2
 target_dir=$3
 
+shopt -s extglob
+
 for f in ${source_dir}/*.cxx
 do
     find_packages=$(python WhatModulesVTK.py ${vtk_source_dir} ${f})
@@ -18,6 +20,10 @@ do
     target_path=${target_dir}/${name}
     mkdir -p ${target_path}
     cp ${f} ${target_path}
+    for addon_file in ${source_dir}/${name}!(.md)
+    do
+        cp ${addon_file} ${target_path}
+    done
     cp CMakeLists.txt.template ${target_path}/CMakeLists.txt
     cp index.html.template ${target_path}/index.html
     sed -i "s/XXX/${name}/g" ${target_path}/index.html
